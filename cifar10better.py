8fc4786fb5d73618b088cf068fc60bddfc7351f0
Victor Cortez <ctovictor@gmail.com>
2017-06-26 07:09:52 -0300

    bigger epoch

--- cifar10better.py
+++ cifar10better.py
@@ -8,7 +8,7 @@ from scipy import misc
 
 
 batch_size = 32 # in each iteration, we consider 32 training examples at once
-num_epochs = 4 # we iterate 200 times over the entire training set
+num_epochs = 15 # we iterate 200 times over the entire training set
 kernel_size = 3 # we will use 3x3 kernels throughout
 pool_size = 2 # we will use 2x2 pooling throughout
 conv_depth_1 = 32 # we will initially have 32 kernels per conv. layer...
